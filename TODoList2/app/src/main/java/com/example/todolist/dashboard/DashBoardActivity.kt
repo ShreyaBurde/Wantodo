372177feb44e8110fe3ec822657957888903f857
ShreyaBurde <108715371+ShreyaBurde@users.noreply.github.com>
2022-07-19 04:51:10 +0530

    project init

---
+++ TODoList2/app/src/main/java/com/example/todolist/dashboard/DashBoardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.todolist.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todolist.R
+
+class DashBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+    }
+}
---
+++ TODoList2/app/src/main/java/com/example/todolist/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.example.todolist.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.SearchEvent
+import com.example.todolist.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+
+
+}
